de0dc09c3904d8f97590a1b6998b18527c73f3ee
Shawn.J.Tseng (tw mis.twtc01.Newegg) 61878 <st7j@buyabs.corp>
2019-01-31 15:54:43 +0800

    實驗: 靜態資源管理image

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 import './style.css';
 import _ from 'lodash';
+import Icon from './icon.png';
 
 function component() {
   let element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  // Add the image to our existing div.
+  var myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
