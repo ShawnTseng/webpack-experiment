d1997c67f94b76c1ae0cb2cf5ff626715742ac9f
shawntseng <shawntseng40@gmail.com>
2020-04-19 08:10:17 +0800

    實驗: 靜態資源管理 Fonts

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,25 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  output: {filename: 'bundle.js', path: path.resolve(__dirname, 'dist')},
-  module:{
-    rules:[
+  output: { filename: 'bundle.js', path: path.resolve(__dirname, 'dist') },
+  module: {
+    rules: [
       {
-        test:/\.css$/,
-        use:[
+        test: /\.css$/,
+        use: [
           'style-loader',
           'css-loader'
         ]
       },
       {
-        test:/\.(png|svg|jpg|gif)$/,
-        use:[
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
           'file-loader'
         ]
       }
