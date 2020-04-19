bf5d8fbdcfb9b0f4d0dd3ead428094ce130be87f
shawntseng <shawntseng40@gmail.com>
2020-04-19 08:34:44 +0800

    實驗: 靜態資源管理 Data

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,18 @@ module.exports = {
         use: [
           'file-loader'
         ]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: [
+          'csv-loader'
+        ]
+      },
+      {
+        test: /\.xml$/,
+        use: [
+          'xml-loader'
+        ]
       }
     ]
   }
